e5fe292aa8b53d004a648bc8d0f9640b63cf11cb
Pooja bhalla <poojabhalla122@gmail.com>
2025-06-15 19:52:48 +0530

    updation firebase

--- api/firestore.py
+++ api/firestore.py
@@ -1,7 +1,11 @@
 import firebase_admin
+import os, json
+
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("firebase-key.json")
+firebase_json = os.environ.get("FIREBASE_KEY_JSON")
+cred = credentials.Certificate(json.loads(firebase_json))
+
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
